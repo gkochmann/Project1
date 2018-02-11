44eb06ba5eb8bc7618c7900c93362ff677bb1d1c
grant <grant@grant-VirtualBox>
2018-02-10 22:51:39 -0700

    Initial Project Setup

---
+++ Project1/RoundRobin.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RoundRobin.h
+ * Author: grant
+ *
+ * Created on February 10, 2018, 10:47 PM
+ */
+
+#ifndef ROUNDROBIN_H
+#define ROUNDROBIN_H
+
+
+
+#endif /* ROUNDROBIN_H */
+
---
+++ Project1/ShortestProcessNext.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ShortestProcessNext.h
+ * Author: grant
+ *
+ * Created on February 10, 2018, 10:48 PM
+ */
+
+#ifndef SHORTESTPROCESSNEXT_H
+#define SHORTESTPROCESSNEXT_H
+
+
+
+#endif /* SHORTESTPROCESSNEXT_H */
+
